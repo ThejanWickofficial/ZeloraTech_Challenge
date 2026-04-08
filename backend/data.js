2bf15784ba4caa14cc66881763068838274508bd
ThejanWickofficial <dt23thejan@gmail.com>
2026-04-08 13:40:05 +0530

    data: add mock data in to data.js file

---
+++ backend/data.js
@@ -0,0 +1,31 @@
+let candidates = [
+    {
+        id: "1",
+        name: "Marlon Reynolds",
+        stage: "Applying Period",
+        appliedDate: "29 Oct, 2023",
+        overallScore: 3.5,
+        isReferred: true,
+        hasAssessment: false
+    },
+    {
+        id: "2",
+        name: "Kristi Sipes",
+        stage: "Screening",
+        appliedDate: "20 Oct, 2023",
+        overallScore: 3.5,
+        isReferred: false,
+        hasAssessment: false
+    },
+    {
+        id: "3",
+        name: "Cameron Dickens",
+        stage: "Interview",
+        appliedDate: "03 Sep, 2023",
+        overallScore: 4.0,
+        isReferred: false,
+        hasAssessment: false
+    }
+];
+
+module.exports = { candidates };
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 
-const app = express();
+let { candidates } = require("./data");
 
+const app = express();
 const PORT = 5001;
 
 app.use(cors());
