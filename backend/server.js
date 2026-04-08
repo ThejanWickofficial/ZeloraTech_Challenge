44cd044af89e4f268e08244e23bf954e206e1e20
ThejanWickofficial <dt23thejan@gmail.com>
2026-04-08 11:58:52 +0530

    feat: import express and set the server to listen from PORT: 5001

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+const PORT = 5001;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/test", (req,res) =>{
+    res.json({message: "ZeloraTech backend run successfully!!"});
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running On PORT number: ${PORT}`);
+});
